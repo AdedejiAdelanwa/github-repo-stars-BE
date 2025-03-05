d01b465fc0bf0c0086e997026edac8f3ba8a3365
Adedeji Adelanwa <adedejiadelanwa@gmail.com>
2025-03-05 01:11:23 +0100

    chores: Remove files

--- dist/configs/app.config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-export const appConfig = {
-    port: process.env.PORT || 3100,
-    githubToken: process.env.GITHUB_TOKEN,
-    timezone: process.env.TIMEZONE,
-    githubApiUrl: process.env.GITHUB_API_URL,
-};
--- dist/exceptions.js
+++
@@ -1,6 +0,0 @@
-export class RepoNotFound extends Error {
-}
-export class OwnerNotFound extends Error {
-}
-export class AuthorizationError extends Error {
-}
